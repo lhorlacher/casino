c2095c8c37ad73f2d5df79edf6a79ae626e442fe
Lonnie Horlacher <horlacher.lonnie@gmail.com>
2016-08-08 12:46:49 -0600

    notes and gems

---
+++ casino_notes.rb
@@ -0,0 +1,47 @@
+
+
+
+# General Notes:
+	# quit from anywhere
+
+
+#classes
+	#person
+			#description
+				# @first_name
+				# @last_name
+				# @luck level (if we have time)
+	#bankroll (LONNIE)
+			#bankroll
+				# @bankroll
+				# Set balance
+				# Check balance
+				# Change balance (manual and auto version)
+	#games (We may not need this.)
+		#cards
+			# generate deck
+			# deal card(s) (must remove from dealable cards)
+			# hold dealable cards
+			#card games
+				# 21 (blackjack)
+				# Blitz
+				# POKER!!!
+
+		#dice
+			# @number of dice
+			# roll dice
+			# dice
+				# dice games
+					# Craps
+
+		#chance
+			# chance games
+				# Slots
+				# High / Low (ANDREW)
+
+	#casino (RYAN)
+			# switch player
+			# switch games
+			# borrow money (if we have time)
+			# betting
+			# Game change events
